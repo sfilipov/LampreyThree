ece1c39b97e51803f1766afa533a849f26aed3d8
David Morris <davidrowlandmorris@gmail.com>
2012-12-02 21:22:50 +0000

    Cull comment templates from tests

--- src/test/java/eel/seprphase2/Simulator/ReactorTest.java
+++ src/test/java/eel/seprphase2/Simulator/ReactorTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package eel.seprphase2.Simulator;
 
 import eel.seprphase2.Utilities.Percentage;
--- src/test/java/eel/seprphase2/Utilities/PressureTest.java
+++ src/test/java/eel/seprphase2/Utilities/PressureTest.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package eel.seprphase2.Utilities;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *

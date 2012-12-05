d9d7b53ebd4ee008d2b162c2d32af23e1388f087
Marius Dumitrescu <md722@york.ac.uk>
2012-12-05 10:48:58 +0000

    reactor and turbine inherit failablecomponent. added failablecomponent and states

---
+++ src/main/java/FailureModel/FailableComponent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FailureModel;
+
+/**
+ *
+ * @author Yazidi
+ */
+public abstract class FailableComponent {
+    private FailureState failureState;
+    
+    public FailableComponent()
+    {
+        failureState = FailureState.Normal;
+    }
+    
+    public FailureState getFailureState()
+    {
+        return this.failureState;
+    }
+}
---
+++ src/test/java/FailureModel/FailableComponentTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FailureModel;
+
+import eel.seprphase2.Simulator.Reactor;
+import eel.seprphase2.Simulator.Turbine;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+
+/**
+ *
+ * @author Yazidi
+ */
+public class FailableComponentTest {
+    
+   
+    
+   
+    
+    
+    
+    public FailableComponentTest() {
+    }
+    
+    @Test
+    public void shouldInitializeReactorComponentStateToNotFailed() {
+        
+        FailableComponent reactor;
+        reactor = new Reactor();
+        assertEquals(FailureState.Normal,reactor.getFailureState());
+    }
+    
+    @Test
+    public void shouldInitializeTurbineComponentStateToNotFailed() {
+        FailableComponent turbine;
+        turbine = new Turbine();
+        assertEquals(FailureState.Normal,turbine.getFailureState());
+    }
+}

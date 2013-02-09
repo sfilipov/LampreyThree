b4277b4bbfe291866744dd76c8fd95e62a045681
Simeon Filipov <ssfilipov@gmail.com>
2013-02-09 13:01:15 +0000

    Add new menu images

--- src/main/java/lamprey/seprphase3/GUI/BackyardReactor.java
+++ src/main/java/lamprey/seprphase3/GUI/BackyardReactor.java
@@ -17,16 +17,26 @@ import com.badlogic.gdx.math.Rectangle;
  * @author Simeon
  */
 public class BackyardReactor implements ApplicationListener {
-    Texture menuImage;
-    Texture logoImage;
+    Texture menubgImage;
+    Texture gamelogoImage;
+    Texture newgameImage;
+    Texture loadgameImage;
+    Texture optionsImage;
+    Texture highscoresImage;
+    Texture lampreylogoImage;
     OrthographicCamera camera;
     SpriteBatch batch;
     Rectangle menu;
     
     @Override
     public void create() {
-        menuImage = new Texture(Gdx.files.internal("assets\\menu.png"));
-        logoImage = new Texture(Gdx.files.internal("assets\\logo.png"));
+        menubgImage      = new Texture(Gdx.files.internal("assets\\menubg.png"));
+        gamelogoImage    = new Texture(Gdx.files.internal("assets\\gamelogo.png"));
+        newgameImage     = new Texture(Gdx.files.internal("assets\\newgame.png"));
+        loadgameImage    = new Texture(Gdx.files.internal("assets\\loadgame.png"));
+        optionsImage     = new Texture(Gdx.files.internal("assets\\options.png"));
+        highscoresImage  = new Texture(Gdx.files.internal("assets\\highscores.png"));
+        lampreylogoImage = new Texture(Gdx.files.internal("assets\\lampreylogo.png"));
         
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 960, 540);
@@ -52,7 +62,13 @@ public class BackyardReactor implements ApplicationListener {
         
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(menuImage, menu.x, menu.y);
+        batch.draw(menubgImage,      0,   0);
+        batch.draw(gamelogoImage,    272, 333);
+        batch.draw(newgameImage,     361, 283);
+        batch.draw(loadgameImage,    349, 241);
+        batch.draw(optionsImage,     392, 199);
+        batch.draw(highscoresImage,  349, 155);
+        batch.draw(lampreylogoImage, 436, 0);
         batch.end();
     }
 

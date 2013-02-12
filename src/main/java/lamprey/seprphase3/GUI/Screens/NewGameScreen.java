333a104439dfbb4c189538b8f37b20b01d2408e6
Simeon Filipov <ssfilipov@gmail.com>
2013-02-12 15:42:41 +0000

    Create NewGameScreen

---
+++ src/main/java/lamprey/seprphase3/GUI/Screens/NewGameScreen.java
@@ -0,0 +1,14 @@
+package lamprey.seprphase3.GUI.Screens;
+
+import lamprey.seprphase3.GUI.BackyardReactor;
+
+/**
+ *
+ * @author Simeon
+ */
+public class NewGameScreen extends AbstractScreen {
+    
+    public NewGameScreen(BackyardReactor game) {
+        super(game);
+    }
+}

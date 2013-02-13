b5007d492f2b2094afa8f90051cfc8c89c232151
Simeon Filipov <ssfilipov@gmail.com>
2013-02-13 20:11:33 +0000

    Make game window unresizable

--- src/main/java/lamprey/seprphase3/GUI/GameStarter.java
+++ src/main/java/lamprey/seprphase3/GUI/GameStarter.java
@@ -13,7 +13,7 @@ public class GameStarter {
         cfg.title  = "Backyard Reactor";
         cfg.width  = 960;
         cfg.height = 540;
-        cfg.resizable = true;
+        cfg.resizable = false;
         cfg.useGL20   = true;
         
         new LwjglApplication(new BackyardReactor(), cfg);

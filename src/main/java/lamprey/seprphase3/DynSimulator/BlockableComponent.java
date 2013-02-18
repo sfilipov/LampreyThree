d363b5a513499b8a27d9ac0af6939258999f432e
WillFrew <will.frew1@gmail.com>
2013-02-18 19:28:47 +0000

    Create BlockableComponent abstract class

---
+++ src/main/java/lamprey/seprphase3/DynSimulator/BlockableComponent.java
@@ -0,0 +1,14 @@
+package lamprey.seprphase3.DynSimulator;
+
+/**
+ *
+ * @author will
+ */
+public abstract class BlockableComponent extends FlowThroughComponent {
+    
+    /**
+     * Whether or not this component is blocked.
+     */
+    public boolean blocked;
+    
+}

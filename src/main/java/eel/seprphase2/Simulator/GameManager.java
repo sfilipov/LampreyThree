772fb7a5a45ba05e445832c6a069c8d2ab17502b
James Murkin <jamesmurkin@hotmail.com>
2013-02-19 17:30:14 +0000

    Comment on gamemanager

--- src/main/java/eel/seprphase2/Simulator/GameManager.java
+++ src/main/java/eel/seprphase2/Simulator/GameManager.java
@@ -3,11 +3,11 @@ package eel.seprphase2.Simulator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
- *
+ *Interface class that defines the methods that'll be needed in order to save and load the game.
  * @author David
  */
 public interface GameManager {
-
+    
     String[] listGames();
 
     void loadGame(int gameNumber);

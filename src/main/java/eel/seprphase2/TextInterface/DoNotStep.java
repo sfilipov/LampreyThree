f6fca4d1b59b7f7ceb7570b728d5a4f3790dcdcb
WillFrew <will.frew1@gmail.com>
2013-02-20 20:18:23 +0000

    Remove old redundant Eel code

--- src/main/java/eel/seprphase2/TextInterface/DoNotStep.java
+++
@@ -1,8 +0,0 @@
-package eel.seprphase2.TextInterface;
-
-/**
- * Exception thrown when the interface should not step after a command
- * @author James
- */
-public class DoNotStep extends Exception {
-}
